5a166750e3184541025b3ac5dd7d48e388bb0fa1
Ben Greear <greearb@candelatech.com>
2014-08-05 13:24:48 -0700

    ath10k: support rx-software-crypt mode.
    With appropriate firmware (only CT firmware at this time)
    this allows enabling rx-software-crypt mode.  This tells the
    firmware to not decrypt any packets received, but do encrypt
    (as needed) any packets on transmit.  This is implemented to get
    around hardware issues that preclude using two stations to
    connect to the same AP while using encryption.
    
    Always have to ignore FCS when actually using encryption since
    firmware marks decrypt errors as FCS errors.  Decrypt logic
    should catch any real problems.
    
    Add firmware feature flag to specify if firmware supports
    this feature.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/mac.c
+++ drivers/net/wireless/ath/ath10k/mac.c
@@ -19,6 +19,7 @@
 
 #include <net/mac80211.h>
 #include <linux/etherdevice.h>
+#include <linux/module.h>
 
 #include "hif.h"
 #include "core.h"
@@ -205,6 +206,13 @@ int ath10k_mac_ext_resource_config(struct ath10k *ar, u32 val)
 
 	return 0;
 }
+
+/* 0:  Full hardware crypt
+ * 1:  Tx hardware crypt, but expect rx software crypt (use native wifi tx type)
+ */
+int ath10k_modparam_nohwcrypt;
+module_param_named(nohwcrypt, ath10k_modparam_nohwcrypt, int, 0444);
+MODULE_PARM_DESC(nohwcrypt, "Disable hardware rx decrypt feature");
 
 /**********/
 /* Crypto */
