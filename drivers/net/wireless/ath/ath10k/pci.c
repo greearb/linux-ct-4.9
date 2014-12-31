685b2eeba7aabaf0990c6c97ce79eccd766c9ef5
Ben Greear <greearb@candelatech.com>
2014-12-31 13:45:55 -0800

    ath10k: add fw-powerfup-fail to ethtool stats.
    This gives user-space a normal-ish way to detect that
    firmware has failed to start and that a reboot is
    probably required.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/pci.c
+++ drivers/net/wireless/ath/ath10k/pci.c
@@ -2736,10 +2736,13 @@ static int ath10k_pci_hif_power_up(struct ath10k *ar)
 	}
 	napi_enable(&ar->napi);
 
+	ar->fw_powerup_failed = false;
+
 	return 0;
 
 err_ce:
 	ath10k_pci_ce_deinit(ar);
+	ar->fw_powerup_failed = true;
 
 err_sleep:
 	return ret;
