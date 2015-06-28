81cf4038309600af9cf9e9ddfffbd3f97eb13621
Ben Greear <greearb@candelatech.com>
2015-06-28 09:06:56 -0700

    ath10k: Support 'special' pdev cmd-id.
    This is for CT firmware only.  Supports some register
    overrides (thresh62_ext, ack timing, etc).
    
    debugfs interface is added to modify thresh62_ext
    value.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/core.h
+++ drivers/net/wireless/ath/ath10k/core.h
@@ -989,6 +989,8 @@ struct ath10k {
 	struct net_device napi_dev;
 	struct napi_struct napi;
 
+	u8 ath10k_thresh62_ext; /* be sure to flush this to firmware after resets */
+
 	/* must be last */
 	u8 drv_priv[0] __aligned(sizeof(void *));
 };
