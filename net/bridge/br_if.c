a0507ad1fc5cf4d12fb83bf7dd93d47c325f7699
Ben Greear <greearb@candelatech.com>
2015-07-13 16:07:40 -0700

    bridge: Do not enable PROMISC mode when adding AP.
    This fixes ath10k instability in PROMISC mode, and should
    improve bridged AP performance in busy environments.
    
    See this for original idea and description:
    
    http://lists.celinuxforum.org/pipermail/bridge/2008-June/005906.html
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- net/bridge/br_if.c
+++ net/bridge/br_if.c
@@ -93,7 +93,8 @@ static void br_port_set_promisc(struct net_bridge_port *p)
 	if (br_promisc_port(p))
 		return;
 
-	err = dev_set_promiscuity(p->dev, 1);
+	if (!p->dev->ieee80211_ptr)
+		err = dev_set_promiscuity(p->dev, 1);
 	if (err)
 		return;
 
@@ -120,7 +121,8 @@ static void br_port_clear_promisc(struct net_bridge_port *p)
 	if (err)
 		return;
 
-	dev_set_promiscuity(p->dev, -1);
+	if (!p->dev->ieee80211_ptr)
+		dev_set_promiscuity(p->dev, -1);
 	p->flags &= ~BR_PROMISC;
 }
 
@@ -185,8 +187,10 @@ static void nbp_delete_promisc(struct net_bridge_port *p)
 	 * from it.
 	 */
 	dev_set_allmulti(p->dev, -1);
-	if (br_promisc_port(p))
-		dev_set_promiscuity(p->dev, -1);
+	if (br_promisc_port(p)) {
+		if (!p->dev->ieee80211_ptr)
+			dev_set_promiscuity(p->dev, -1);
+	}
 	else
 		br_fdb_unsync_static(p->br, p);
 }
