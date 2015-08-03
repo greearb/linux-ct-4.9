842d4d217e34ae05f400c95b6a58de4cde4729a4
Ben Greear <greearb@candelatech.com>
2015-08-03 16:11:42 -0700

    ath10k: Add compatibility with older CT firmware.
    And fix a mis-ported FW feature to match existing
    (and future) firmware.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/core.h
+++ drivers/net/wireless/ath/ath10k/core.h
@@ -618,6 +618,9 @@ enum ath10k_fw_features {
 	 */
 	ATH10K_FW_FEATURE_SKIP_NULL_FUNC_WAR = 15,
 
+	/* tx-status has the noack bits (CT firmware version 14 and higher ) */
+	ATH10K_FW_FEATURE_HAS_TXSTATUS_NOACK = 30,
+
 	/* Firmware from Candela Technologies, enables more VIFs, etc */
 	ATH10K_FW_FEATURE_WMI_10X_CT = 31,
 
@@ -625,9 +628,6 @@ enum ath10k_fw_features {
 	 * Required for multiple stations connected to same AP when using
 	 * encryption (ie, commercial version of CT firmware) */
 	ATH10K_FW_FEATURE_CT_RXSWCRYPT = 32,
-
-	/* tx-status has the noack bits (CT firmware version 14 and higher ) */
-	ATH10K_FW_FEATURE_HAS_TXSTATUS_NOACK = 33,
 
 	/* keep last */
 	ATH10K_FW_FEATURE_COUNT,
