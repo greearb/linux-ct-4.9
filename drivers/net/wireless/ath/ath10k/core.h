40dd3b1f6dc886cc8fcfc9f47f63cc98670330c3
Ben Greear <greearb@candelatech.com>
2015-09-25 17:19:00 -0700

    ath10k: Support enabling AMSDU on IBSS interfaces (CT only).
    This only works with CT firmware (beta-15 and later).
    
    Ath10k hardware sets the BSSID to zero for AMSDU frames on
    IBSS interfaces, which appears to be against spec.  You have
    to hack the receiving stack to ignore the bssid mismatch or
    the frames will just be dropped.
    
    By default, AMSDU is still disabled for IBSS interfaces.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/core.h
+++ drivers/net/wireless/ath/ath10k/core.h
@@ -1016,6 +1016,7 @@ struct ath10k {
 
 	/* Index 0 is for 5Ghz, index 1 is for 2.4Ghz, CT firmware only. */
 	/* be sure to flush this to firmware after resets */
+	/* Includes various other backdoor hacks as well. */
 	struct {
 		struct {
 #define MIN_CCA_PWR_COUNT 3
@@ -1027,6 +1028,7 @@ struct ath10k {
 			u8 enable_minccapwr_thresh;
 		} bands[2];
 		u8 thresh62_ext;
+		bool allow_ibss_amsdu;
 	} eeprom_overrides;
 
 	/* must be last */
