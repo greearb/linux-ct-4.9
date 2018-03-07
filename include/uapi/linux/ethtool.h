33e83d88797a7238130fe68977bd5efbef361184
Ben Greear <greearb@candelatech.com>
2018-03-07 11:47:00 -0800

    ethtool: Support ETHTOOL_GSTATS2 command.
    This is similar to ETHTOOL_GSTATS, but it allows you to specify
    a 'level'.  This level can be used by the driver to decrease the
    amount of stats refreshed.  In particular, this helps with ath10k
    since getting the firmware stats can be slow.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- include/uapi/linux/ethtool.h
+++ include/uapi/linux/ethtool.h
@@ -1315,6 +1315,10 @@ struct ethtool_per_queue_op {
 #define ETHTOOL_SLINKSETTINGS	0x0000004d /* Set ethtool_link_settings */
 
 
+#define ETHTOOL_GSTATS2		0x0000ff01 /* get NIC-specific statistics
+					    * with ability to specify level
+					    */
+
 /* compatibility with older code */
 #define SPARC_ETH_GSET		ETHTOOL_GSET
 #define SPARC_ETH_SSET		ETHTOOL_SSET
